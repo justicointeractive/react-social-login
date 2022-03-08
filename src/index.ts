5e04855832a3d5a57a0ae58a7c1516843ce73c22
Joe Flateau <joe@joeflateau.net>
2022-03-08 17:03:19 -0500

    only attempt load once

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,13 @@ export type SDKOptionsType<T> = T extends SDKImplementation<infer TOps>
   ? TOps
   : never;
 
+const providerLoad = new Map<keyof SDKImplementations, Promise<void>>();
+
 export async function socialLogin<K extends keyof SDKImplementations>(
   provider: K,
   options: SDKOptionsType<SDKImplementations[K]>
 ) {
-  const sdk = sdks[provider];
-  await sdk.load(options as any);
+  const sdk = await loadProviderOnce<K>(provider, options);
   const result = await sdk.login(options as any);
   return sdk.generateUser(result);
 }
@@ -21,7 +22,17 @@ export async function socialLogout<K extends keyof SDKImplementations>(
   provider: K,
   options: SDKOptionsType<SDKImplementations[K]>
 ) {
-  const sdk = sdks[provider];
-  await sdk.load(options as any);
+  const sdk = await loadProviderOnce<K>(provider, options);
   await sdk.logout();
+}
+
+export async function loadProviderOnce<K extends keyof SDKImplementations>(
+  provider: K,
+  options: SDKOptionsType<SDKImplementations[K]>
+) {
+  const sdk = sdks[provider];
+  const loadPromise = providerLoad.get(provider) ?? sdk.login(options as any);
+  providerLoad.set(provider, loadPromise);
+  await loadPromise;
+  return sdk;
 }
