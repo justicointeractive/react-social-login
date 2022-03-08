a2638697049a590ff6997ce6fda37c63aef2b755
Joe Flateau <joe@joeflateau.net>
2022-03-08 16:00:03 -0500

    simpler generics for basic methods

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,20 @@ type SDKImplementations = typeof sdks;
 
 type SDKOptionsType<T> = T extends SDKImplementation<infer TOps> ? TOps : never;
 
-export async function socialLogin<
-  T extends SDKImplementations,
-  K extends keyof SDKImplementations
->(provider: K, options: SDKOptionsType<T[K]>) {
+export async function socialLogin<K extends keyof SDKImplementations>(
+  provider: K,
+  options: SDKOptionsType<SDKImplementations[K]>
+) {
   const sdk = sdks[provider];
   await sdk.load(options as any);
   const result = await sdk.login(options as any);
   return sdk.generateUser(result);
 }
 
-export async function socialLogout<
-  T extends SDKImplementations,
-  K extends keyof SDKImplementations
->(provider: K, options: SDKOptionsType<T[K]>) {
+export async function socialLogout<K extends keyof SDKImplementations>(
+  provider: K,
+  options: SDKOptionsType<SDKImplementations[K]>
+) {
   const sdk = sdks[provider];
   await sdk.load(options as any);
   await sdk.logout();
